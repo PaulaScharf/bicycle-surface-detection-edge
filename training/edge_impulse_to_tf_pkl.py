94ab7579119f2090861611e5cc75d49b4097ac6b
PaulaScharf <paulascharf@gmx.de>
2024-04-12 10:06:02 +0200

    update recording frequency

--- training/edge_impulse_to_tf_pkl.py
+++ training/edge_impulse_to_tf_pkl.py
@@ -6,8 +6,8 @@ from sklearn.model_selection import train_test_split
 import pickle
 from sklearn.preprocessing import StandardScaler
 
-window_size = int(2000/17.24137931034483)
-window_increase = int(400/17.24137931034483)
+window_size = int(3*31.25) # window is 3 seconds big
+window_increase = int(0.4*31.25)
 
 with open('training/trainingsdata/info.labels.json', 'r') as f:
   paths_labels = json.load(f)
@@ -38,20 +38,20 @@ for file_info in paths_labels["files"]:
     statistics.append(statistics_row)
 
 df = pd.DataFrame(statistics, columns=['Label',
-                                       'AccX_Average', 'AccX_Min', 'AccX_Max', 'AccX_Std Deviation', 'AccX_RMS', 'AccX_Skewness', 'AccX_Kurtosis',
-                                       'AccY_Average', 'Min', 'Max', 'Std Deviation', 'RMS', 'Skewness', 'Kurtosis',
-                                       'AccZ_Average', 'Min', 'Max', 'Std Deviation', 'RMS', 'Skewness', 'Kurtosis',
-                                       'GyrX_Average', 'Min', 'Max', 'Std Deviation', 'RMS', 'Skewness', 'Kurtosis',
-                                       'GyrY_Average', 'Min', 'Max', 'Std Deviation', 'RMS', 'Skewness', 'Kurtosis',
-                                       'GyrZ_Average', 'Min', 'Max', 'Std Deviation', 'RMS', 'Skewness', 'Kurtosis'])
+                                       'AccX_Average', 'AccX_Min', 'AccX_Max', 'AccX_Std', 'AccX_RMS', 'AccX_Skewness', 'AccX_Kurtosis',
+                                       'AccY_Average', 'AccY_Min', 'AccY_Max', 'AccY_Std', 'AccY_RMS', 'AccY_Skewness', 'AccY_Kurtosis',
+                                       'AccZ_Average', 'AccZ_Min', 'AccZ_Max', 'AccZ_Std', 'AccZ_RMS', 'AccZ_Skewness', 'AccZ_Kurtosis',
+                                       'GyrX_Average', 'GyrX_Min', 'GyrX_Max', 'GyrX_Std', 'GyrX_RMS', 'GyrX_Skewness', 'GyrX_Kurtosis',
+                                       'GyrY_Average', 'GyrY_Min', 'GyrY_Max', 'GyrY_Std', 'GyrY_RMS', 'GyrY_Skewness', 'GyrY_Kurtosis',
+                                       'GyrZ_Average', 'GyrZ_Min', 'GyrZ_Max', 'GyrZ_Std', 'GyrZ_RMS', 'GyrZ_Skewness', 'GyrZ_Kurtosis'])
 
 data = df.iloc[:, 1:].values
 labels = df.iloc[:, 0].values
 
 
-# TODO: Luca normalised the data in his code. Why?? Do we need to do that?
-scaler = StandardScaler()
-data = scaler.fit_transform(data)
+# TODO: Normalizing improves accuracy, but then I would also have to normalise during deploymet, right?
+# scaler = StandardScaler()
+# data = scaler.fit_transform(data)
 
 # Daten in Trainings- und Testsets aufteilen
 data_train, data_test, labels_train, labels_test = train_test_split(data, labels, test_size=0.15, random_state=42)
